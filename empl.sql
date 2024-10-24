c830d6ffb908c68786d74310c8b3fdfc8b45ae01
anenigmabeyondwords <98433698+anenigmabeyondwords@users.noreply.github.com>
2024-10-24 11:46:51 -0400

    Update empl.sql

--- empl.sql
+++ empl.sql
@@ -1,2 +1,2 @@
-SELECT * FROM Employees
-WHERE employee_id > 5 and email_address LIKE '%@gmail.com%
+ SELECT * FROM Employees
+WHERE employee_id between 5 and 10 and email_address = 'john.q.public@yahoo.com'
